82be8bf76edd5ea2966844422408ae4e9a630951
Lorraine Kupa <lorrainekupa@gmail.com>
2023-07-26 09:25:40 +0300

    Updated database to postgresql

--- rentalBackend/settings.py
+++ rentalBackend/settings.py
@@ -73,10 +73,21 @@ WSGI_APPLICATION = 'rentalBackend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'rentalsDB', 
+        'USER': 'kupa', 
+        'PASSWORD': 'kupa@rent2023',
+        'HOST': '127.0.0.1', 
+        'PORT': '5432',
     }
 }
 
