938ef27af80640376fad4f1b968f0bdcb2e85313
Lorraine Kupa <lorrainekupa@gmail.com>
2023-07-26 12:12:08 +0300

    Added postgresql db

--- rentalBackend/settings.py
+++ rentalBackend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-8j+=ff@&9#kfys28-d-t5tzu0epc-mdxao2)cl2ml9ogkh(!)7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -73,21 +73,20 @@ WSGI_APPLICATION = 'rentalBackend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#    }
+#DATABASES = {
+ #   'default': {
+ #       'ENGINE': 'django.db.backends.sqlite3',
+ #   }
 #}
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'rentalsDB', 
-        'USER': 'kupa', 
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'rentalsdb',
+        'USER': 'kupa',
         'PASSWORD': 'kupa@rent2023',
-        'HOST': '127.0.0.1', 
-        'PORT': '5432',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
