b64047b82e7cc68d36ce24c8bb8ee25fa82b31cf
Lorraine Kupa <lorrainekupa@gmail.com>
2023-07-27 14:39:11 +0300

    updated maintenanceRequest views

--- maintenanceRequest/views.py
+++ maintenanceRequest/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets #for CRUD operations
+from .serializers import MaintenanceRequestSerializer
+from .models import MaintenanceRequest
 # Create your views here.
+
+class MaintenanceRequestView(viewsets.ModelViewSet):
+    serializer_class = MaintenanceRequestSerializer
+    queryset = MaintenanceRequest.objects.all()
