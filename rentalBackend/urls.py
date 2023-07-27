e4e0c14d0b075f03854965dae18847082770e03c
Lorraine Kupa <lorrainekupa@gmail.com>
2023-07-27 11:38:46 +0300

    setup agreement api

--- rentalBackend/urls.py
+++ rentalBackend/urls.py
@@ -15,8 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+##Added import
+from rest_framework import routers
+from agreement import views
+
+##Setting up router(api)
+router = routers.DefaultRouter()
+router.register(r'agreements',views.AgreementView, 'agreement')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('agreement/',include(router.urls)),
 ]
