c838a1cfc4a17df2422b660e0f3576acdaf06be0
Lorraine Kupa <lorrainekupa@gmail.com>
2023-07-27 12:18:23 +0300

    Updated communication views

--- communication/views.py
+++ communication/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets #for CRUD operations
+from .serializers import CommunicationSerializer
+from .models import Communication
 # Create your views here.
+
+class CommunicationView(viewsets.ModelViewSet):
+    serializer_class = CommunicationSerializer
+    queryset = Communication.objects.all()
