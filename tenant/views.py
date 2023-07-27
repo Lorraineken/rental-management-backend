a4474167e8e88a24acc10565b41ee782b910e7a7
Lorraine Kupa <lorrainekupa@gmail.com>
2023-07-27 15:00:16 +0300

    updated tenant view

--- tenant/views.py
+++ tenant/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets #for CRUD operations
+from .serializers import TenantSerializer
+from .models import Tenant
 # Create your views here.
+
+class TenantView(viewsets.ModelViewSet):
+    serializer_class = TenantSerializer
+    queryset = Tenant.objects.all()
