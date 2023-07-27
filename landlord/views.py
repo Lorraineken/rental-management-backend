ae7f207438c6fad96320bb265f1871a7ade004ad
Lorraine Kupa <lorrainekupa@gmail.com>
2023-07-27 12:54:16 +0300

    Updated landlord views

--- landlord/views.py
+++ landlord/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets #for CRUD operations
+from .serializers import LandlordSerializer
+from .models import Landlord
 # Create your views here.
+
+class LandlordView(viewsets.ModelViewSet):
+    serializer_class = LandlordSerializer
+    queryset = Landlord.objects.all()
