efe1113232fde904160aed8ca813da31408d3817
Lorraine Kupa <lorrainekupa@gmail.com>
2023-07-27 15:21:15 +0300

    Updated payment views

--- payment/views.py
+++ payment/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets #for CRUD operations
+from .serializers import PaymentSerializer
+from .models import Payment
 # Create your views here.
+
+class PaymentView(viewsets.ModelViewSet):
+    serializer_class = PaymentSerializer
+    queryset = Payment.objects.all()
