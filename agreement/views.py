78a9e2800b7934a69542a853b5d98889a546d54c
Lorraine Kupa <lorrainekupa@gmail.com>
2023-07-27 11:18:01 +0300

    Added views for agreement app

--- agreement/views.py
+++ agreement/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import AgreementSerializer
+from .models import Agreement
 
+#The viewsets base class provides the implementation for CRUD operations
+#  by default. This code specifies the serializer_class and the queryset.
 # Create your views here.
+
+class AgreementView(viewsets.ModelViewSet):
+    serializer_class = AgreementSerializer
+    queryset = Agreement.objects.all()
