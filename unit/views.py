28ed3d8109209724278896fcab270031200f6e2e
Lorraine Kupa <lorrainekupa@gmail.com>
2023-07-27 15:11:02 +0300

    Updated Unit view

--- unit/views.py
+++ unit/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets #for CRUD operations
+from .serializers import UnitSerializer
+from .models import Unit
 # Create your views here.
+
+class UnitView(viewsets.ModelViewSet):
+    serializer_class = UnitSerializer
+    queryset = Unit.objects.all()
