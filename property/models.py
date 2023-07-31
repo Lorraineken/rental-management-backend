0a3577f1b2123736548a370c9b6e127c7b814398
Lorraine Kupa <lorrainekupa@gmail.com>
2023-07-31 16:41:42 +0300

    Updated Property model and serializer

--- property/models.py
+++ property/models.py
@@ -1,16 +1,17 @@
 from django.db import models
+from landlord.models import Landlord
 
 # Create your models here.
 class Property(models.Model):
-    address = models.TextField(max_length=500)
+    address = models.TextField(max_length=500,null=False)
     description = models.TextField(max_length=1000)
-    no_of_units = models.IntegerField()
-    rental_price = models.IntegerField()
-    #landlord = models.ForeignKey('Landlord',on_delete=models.SET_NULL, null=True)
+    no_of_units = models.IntegerField(null=False)
+    rental_price = models.IntegerField(null=False)
+    landlord = models.ForeignKey(Landlord,on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         """String for representing the Model object."""
-        return self.address
+        return (f'{self.no_of_units} units, {self.rental_price} Ksh per unit')
 
 
 
