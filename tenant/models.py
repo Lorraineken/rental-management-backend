6f0f6e2992fc76d32c31e2e3828741a52155dc43
Lorraine Kupa <lorrainekupa@gmail.com>
2023-07-31 16:24:10 +0300

    updated unit and tenant serializers

--- tenant/models.py
+++ tenant/models.py
@@ -3,10 +3,10 @@ from django.db import models
 
 # Create your models here.
 class Tenant(models.Model):
-    firstName = models.CharField(max_length=30,null=False)
-    lastName = models.CharField(max_length=30,null=False)
-    idPassport = models.CharField(max_length=50,null=False)
-    phone = models.IntegerField(null=False)
+    first_name = models.CharField(max_length=30,null=False)
+    last_name = models.CharField(max_length=30,null=False)
+    id_passport = models.CharField(max_length=50,null=False)
+    phone_no = models.IntegerField(null=False)
     email = models.EmailField(max_length=254)
     
 
