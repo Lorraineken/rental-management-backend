8a0c1d10fb5ef4cc2aba22039c2e17b4fc40150e
Lorraine Kupa <lorrainekupa@gmail.com>
2023-07-31 10:23:36 +0300

    updated Unit model

--- unit/models.py
+++ unit/models.py
@@ -1,7 +1,19 @@
 from django.db import models
+from property.models import Property
+from tenant.models import Tenant
 
 class Unit(models.Model):
-    status = models.CharField(max_length=50)
-    #property
-    #tenant
+    STATUS_CHOICES = (
+        ('V', 'VACANT'),
+        ('O', 'OCCUPIED'),
+        ('U', 'UNDER MAINTENANCE')
+    )
+    status = models.CharField(max_length=1,choices=STATUS_CHOICES, default='V')
+    property = models.ForeignKey(Property, on_delete=models.CASCADE, default = None)
+    tenant = models.ForeignKey(
+        Tenant, 
+        models.SET_NULL,
+        blank = True,
+        null = True,
+        )
     
