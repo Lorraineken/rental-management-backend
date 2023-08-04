722bef3fc8efbf3b1a46505700a5314e454d13ad
Lorraine Kupa <lorrainekupa@gmail.com>
2023-08-04 11:05:23 +0300

    Updated tenant urls

--- tenant/urls.py
+++ tenant/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from . import views
 
 
-from .views import TenantList
+from .views import TenantCreate,TenantList,TenantDetail,TenantUpdate,TenantDelete
 
 #router = DefaultRouter()
 #router.register(r'tenants',views.TenantView)
@@ -13,6 +13,10 @@ from .views import TenantList
 #]
 
 urlpatterns = [
+    path('create/',TenantCreate.as_view(), name ='create-tenant'),
     path('',TenantList.as_view()),
+    path('<int:pk>/',TenantDetail.as_view(), name ='retrieve-tenant'),
+    path('update/<int:pk>', TenantUpdate.as_view(),name='update-tenant'),
+    path('delete/<int:pk>', TenantDelete.as_view(), name='delete-tenant')
 ]
 
