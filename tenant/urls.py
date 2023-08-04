0325e3c17ef260e539fa4113dd9ec0f56a33a477
Lorraine Kupa <lorrainekupa@gmail.com>
2023-08-04 11:28:33 +0300

    Updated property urls

--- tenant/urls.py
+++ tenant/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from . import views
+#from . import views
 
 
 from .views import TenantCreate,TenantList,TenantDetail,TenantUpdate,TenantDelete
