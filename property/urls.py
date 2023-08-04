a00f8a1d89ef680c65053f62a56c012bd3c2a68d
Lorraine Kupa <lorrainekupa@gmail.com>
2023-08-04 11:48:36 +0300

    Updated landlord urls

--- property/urls.py
+++ property/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
+#from rest_framework.routers import DefaultRouter
 #from . import views
 from .views import PropertyCreate,PropertyList,PropertyDetail,PropertyUpdate,PropertyDelete
 
