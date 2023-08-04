684deb39c1f5d888611439ee58c0e5df5ec1d64d
Lorraine Kupa <lorrainekupa@gmail.com>
2023-08-04 12:02:05 +0300

    Updated unit urls

--- unit/urls.py
+++ unit/urls.py
@@ -1,10 +1,22 @@
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from . import views
+#from rest_framework.routers import DefaultRouter
+#from . import views
+from .views import UnitCreate,UnitList,UnitDetail,UnitUpdate,UnitDelete
 
-router = DefaultRouter()
-router.register(r'units',views.UnitView)
+#router = DefaultRouter()
+#router.register(r'units',views.UnitView)
+
+#urlpatterns = [
+#    path('', include(router.urls)),
+#]
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('create/',UnitCreate.as_view(), name ='create-unit'),
+    path('',UnitList.as_view()),
+    path('<int:pk>/',UnitDetail.as_view(), name ='retrieve-unit'),
+    path('update/<int:pk>', UnitUpdate.as_view(),name='update-unit'),
+    path('delete/<int:pk>', UnitDelete.as_view(), name='delete-unit')
 ]
+
+
+
