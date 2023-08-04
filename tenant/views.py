c5e5a1179060db5951830d3e60b058c8fedb0eb1
Lorraine Kupa <lorrainekupa@gmail.com>
2023-08-04 11:23:47 +0300

    Updated property views with generics

--- tenant/views.py
+++ tenant/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework import viewsets #for CRUD operations
+#from rest_framework import viewsets #for CRUD operations
 from .serializers import TenantSerializer
 from .models import Tenant
 
